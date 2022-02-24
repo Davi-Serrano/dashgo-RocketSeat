2aff5d5ed3cc577fa4c1f517a624225fa9b37b6d
Davi Serrano <daviscardoso2901@gmail.com>
2022-02-24 19:51:06 -0300

    Configurando Mirage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from 'next/app'
-
 import { ChakraProvider } from '@chakra-ui/react'
-import { SidebarDrawerProvider } from '../context/SidebarDrawer'
-import { theme } from '../styles/theme'
+import { theme } from '../styles/theme';
 
+import { SidebarDrawerProvider } from '../context/SidebarDrawer'
+import { makeServer} from '../services/mirage';
+
+if (process.env.NODE_ENV=== 'development' ){
+      makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
